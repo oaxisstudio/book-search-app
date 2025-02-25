aae5a0c2a523e9957dc45560de09dcf29cbfbc29
kaoru-t <oaxisstudio@gmail.com>
2025-02-25 12:31:57 +0000

    feat: モックの初期化処理を改善
    src/mocks/index.ts にモックを有効化する関数を追加
    src/pages/_app.tsx でモックの初期化を useEffect で行うように変更

--- src/mocks/index.ts
+++ src/mocks/index.ts
@@ -3,3 +3,13 @@ import { worker } from "./browser";
 export function setupMocks() {
   worker.start();
 }
+
+export async function enableMocking() {
+  if (process.env.NODE_ENV !== "development") {
+    return;
+  }
+
+  return worker.start({
+    onUnhandledRequest: "bypass",
+  });
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,25 @@
-import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-
-if (process.env.NODE_ENV === "development") {
-  import("../mocks").then(({ setupMocks }) => {
-    setupMocks();
-  });
-}
+import { useEffect, useState } from "react";
+import "@/styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [isMockReady, setIsMockReady] = useState(false);
+
+  useEffect(() => {
+    async function init() {
+      if (process.env.NODE_ENV === "development") {
+        const { enableMocking } = await import("../mocks");
+        await enableMocking();
+      }
+      setIsMockReady(true);
+    }
+    init();
+  }, []);
+
+  if (!isMockReady) {
+    return <div>Loading...</div>;
+  }
+
   return <Component {...pageProps} />;
 }
 
